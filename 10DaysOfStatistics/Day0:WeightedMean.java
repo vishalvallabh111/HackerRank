94d83087703faad1735977a624776f259920fd79
Khaja Anwar Siddiqui <z1778291@students.niu.edu>
2017-06-23 15:06:28 -0500

    Update Day0:WeightedMean.java

--- 10DaysOfStatistics/Day0:WeightedMean.java
+++ 10DaysOfStatistics/Day0:WeightedMean.java
@@ -7,18 +7,15 @@ public class Solution {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int [] nums=new int[n];
-        int [] weights=new int[n];
         for(int i=0;i<n;++i){
             nums[i]=sc.nextInt();
-        }
-        for(int i=0;i<n;++i){
-            weights[i]=sc.nextInt();
         }
         long sum=0;
         long all_weights=0;
         for(int i=0;i<n;++i){
-            sum+=(nums[i]*weights[i]); 
-            all_weights+=weights[i];
+            int weight=sc.nextInt();
+            sum+=(nums[i]*weight); 
+            all_weights+=weight;
         }
         System.out.printf("%.1f%n",(double)sum/all_weights);
     }
